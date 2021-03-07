102c6c231439a943a51699b2617c15f47196cfd6
Alexander Jipa <alexander.jipa@gmail.com>
2021-03-06 22:10:43 -0500

    adding Java template

---
+++ src/Template.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Template {
+    static void solve() {
+
+    }
+
+    //////////////////////// Template ////////////////////////
+    static int readInt() {
+        return Integer.parseInt(readString());
+    }
+
+    static long readLong() {
+        return Long.parseLong(readString());
+    }
+
+    static String readString() {
+        while (tok == null || !tok.hasMoreTokens()) {
+            try {
+                tok = new StringTokenizer(in.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tok.nextToken();
+    }
+
+    static BufferedReader in;
+    static PrintWriter out;
+    static StringTokenizer tok;
+
+    public static void main(String[] arg) {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+        solve();
+        out.close();
+    }
+}
