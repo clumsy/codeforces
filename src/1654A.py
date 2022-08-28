8f18123dd7f9cdc57e8130b9e732f87680fa7e9e
Alexander Jipa <alexander.jipa@gmail.com>
2022-08-27 23:19:39 -0400

    improve heapq imports

--- src/1654A.py
+++ src/1654A.py
@@ -1,12 +1,12 @@
-import heapq
+from heapq import heappush, heappop
 
 t = int(input())
 for _ in range(t):
     _, a = input(), (int(i) for i in input().split())
     h = []
     for i in a:
-        heapq.heappush(h, i)
+        heappush(h, i)
         if len(h) > 2:
-            heapq.heappop(h)
+            heappop(h)
     res = sum(h)
     print(res)
--- src/1660B.py
+++ src/1660B.py
@@ -1,12 +1,12 @@
-import heapq
+from heapq import heappush, heappop
 
 t = int(input())
 for _ in range(t):
     n, a = input(), (int(i) for i in input().split())
     h = []
     for i in a:
-        heapq.heappush(h, i)
+        heappush(h, i)
         if len(h) > 2:
-            heapq.heappop(h)
+            heappop(h)
     res = (len(h) == 1 and h[0] == 1) or (len(h) == 2 and h[1] - h[0] < 2)
     print("YES" if res else "NO")
