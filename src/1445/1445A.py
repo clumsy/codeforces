4a66242b4b1510032c081670c47689733fd22fc8
Alexander Jipa <alexander.jipa@gmail.com>
2023-01-27 21:15:56 -0500

    1445A https://codeforces.com/problemset/problem/1445/A

--- src/1445/1445A.py
+++ src/1445/1445A.py
@@ -1,5 +1,9 @@
 t = int(input())
-for _ in range(t):
-    n = input()
-    res = len(n)
+for k in range(t):
+    n, x = (int(i) for i in input().split())
+    a = (int(i) for i in input().split())
+    b = [int(i) for i in input().split()]
+    res = "YES" if all(i + j <= x for i, j in zip(a, reversed(b))) else "NO"
+    if k < t - 1:
+        input()
     print(res)
