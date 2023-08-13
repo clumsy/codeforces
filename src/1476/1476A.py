ec2a9a09a5cb48c8c63f6847826cec96d81952ba
Alexander Jipa <alexander.jipa@gmail.com>
2023-08-12 23:08:48 -0400

    1476A https://codeforces.com/problemset/problem/1476/A

--- src/1476/1476A.py
+++ src/1476/1476A.py
@@ -1,8 +1,6 @@
 t = int(input())
 for _ in range(t):
-    n = int(input())
-    res = ["9"] * n
-    for i in range(1, n):
-        res[i] = str((i + 7) % 10)
-    res = "".join(res)
+    n, k = (int(i) for i in input().split())
+    k = max(k, k * (n + k - 1) // k)
+    res = (k + n - 1) // n
     print(res)
