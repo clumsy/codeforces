4932de8c5a3bb18c485674856b1524fec6488336
Alexander Zhipa <alex.zhipa@proton.me>
2024-11-14 00:12:28 -0500

    1732A https://codeforces.com/problemset/problem/1732/A

--- src/1732/1732A.py
+++ src/1732/1732A.py
@@ -1,21 +1,34 @@
-from functools import reduce
+def _gcd(a, b):
+    return a if b == 0 else _gcd(b, a % b)
+
+
+def gcd(*v):
+    g = v[0]
+    for i in v[1:]:
+        g = _gcd(g, i)
+    return g
+
 
 t = int(input())
-
-
-def gcd(a, b):
-    return a if b == 0 else gcd(b, a % b)
-
-
 for _ in range(t):
     n, a = int(input()), [int(i) for i in input().split()]
-    res, g = 0, reduce(gcd, a)
-    if g > 1:
-        for i in reversed(range(n)):
-            a[i] = gcd(a[i], i + 1)
-            g_hat = gcd(g, a[i])
-            res += n - i if g_hat < g else 0
-            g = g_hat
-            if gcd(g, a[i]) == 1:
-                break
+    if n == 1:
+        res = 0 if a[0] == 1 else 1
+    elif n == 2:
+        if gcd(*a) == 1:
+            res = 0
+        elif gcd(a[0], gcd(2, a[1])) == 1:
+            res = 1
+        else:
+            res = 2
+    else:
+        g = gcd(*a[:-2])
+        if gcd(g, a[-2], a[-1]) == 1:
+            res = 0
+        elif gcd(g, a[-2], gcd(a[-1], n)) == 1:
+            res = 1
+        elif gcd(g, gcd(n - 1, a[-2]), a[-1]) == 1:
+            res = 2
+        else:
+            res = 3
     print(res)
