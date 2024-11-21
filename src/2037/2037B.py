4cef066037e1d712ee9a6a8500f78809c6c02de2
Alexander Zhipa <alex.zhipa@proton.me>
2024-11-20 22:07:16 -0500

    2037B https://codeforces.com/problemset/problem/2037/B

--- src/2037/2037B.py
+++ src/2037/2037B.py
@@ -1,13 +1,14 @@
-from collections import Counter
-
-
 t = int(input())
 for _ in range(t):
     k, a = int(input()), (int(i) for i in input().split())
-    cnt = Counter(a)
-    for n in cnt:
-        m, r = divmod(k - 2, n)
-        if r == 0 and cnt[m] > (n == m):
-            res = n, m
-            break
+    cnt = [0] * (k + 1)
+    for i in a:
+        cnt[i] += 1
+    k -= 2
+    for n in range(1, k + 1):
+        if k % n == 0 and cnt[n] > 0:
+            m = k // n
+            if cnt[m] > int(n == m):
+                res = n, m
+                break
     print(*res)
