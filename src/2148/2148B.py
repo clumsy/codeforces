a4903cac820dfa51177bec46c8092a6d69aed652
Alexander Zhipa <alex.zhipa@proton.me>
2025-09-19 23:59:20 -0400

    2148B https://codeforces.com/problemset/problem/2148/B

---
+++ src/2148/2148B.py
@@ -0,0 +1,7 @@
+t = int(input())
+for _ in range(t):
+    n, m, x, y = (int(i) for i in input().split())
+    a = [int(i) for i in input().split()]
+    b = [int(i) for i in input().split()]
+    res = n + m
+    print(res)
